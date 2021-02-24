500c24e5f8c1c0af3c92a6951b6df3fbe61f4d20
Daniel Williams <dlw1ll27@gmail.com>
2021-02-24 12:10:34 -0800

    adds air_fryer and cheese Products to database in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-megapants = Product.create({ name: "megapants", price: 200, image_url: "image.url.com", description: "We don't really know what it is, but they're MEGA" })
+# megapants = Product.create({ name: "megapants", price: 200, image_url: "image.url.com", description: "We don't really know what it is, but they're MEGA" })
+
+air_fryer = Product.create({ name: "air_fryer", price: 30, image_url: "image.air_fryer.url.com", description: "Easy bake oven but for adults" })
+
+cheese = Product.create({ name: "cheese", price: 2, image_url: "image.cheese.url.com", description: "come get yer cheese boi" })
