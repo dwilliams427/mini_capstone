0d7429ba952a5135a7719621e8b1a0eb4b9b180c
Daniel Williams <dlw1ll27@gmail.com>
2021-02-24 12:26:29 -0800

    adds individual product pages

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,19 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render "all_products.json.jb"
   end
+
+  def megapants
+    @megapants = Product.find_by(id: 1)
+    render "megapants.json.jb"
+  end
+
+  def air_fryer
+    @air_fryer = Product.find_by(id: 2)
+    render "air_fryer.json.jb"
+  end
+
+  def cheese
+    @cheese = Product.find_by(id: 3)
+    render "cheese.json.jb"
+  end
 end
